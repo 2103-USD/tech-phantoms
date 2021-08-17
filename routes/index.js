3cf21752c9e0229f7d24429323fba30f436c069c
Joel Menezes <menezesj@cajonvalley.net>
2021-08-16 20:31:44 -0700

    Added status.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const apiRouter = require('express').Router();
 
 apiRouter.get("/", (req, res, next) => {
-  res.send({
-    message: "API is under construction!"
-  });
+    res.status(418);
+    res.send({
+        message: "API is under construction!"
+    });
 });
 
 module.exports = apiRouter;
