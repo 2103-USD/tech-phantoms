5b8f7000218039bb0f82fef8bb37a8e07f633ffd
Joel Menezes <menezesj@cajonvalley.net>
2021-08-18 18:39:52 -0700

    commiting shell of API router

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,16 @@
-const apiRouter = require('express').Router();
+// Constants
+const { JWT_SECRET } = process.env;
+
+// Requires
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const apiRouter = express.Router();
+const usersRouter = require ('./users');
+const ordersRouter = require ('./orders');
+const orderProductsRouter = require ('./orders');
+const productsRouter = require ('./products');
+
+const { getUserById } = require('../db');
 
 apiRouter.get("/", (req, res, next) => {
     res.status(418);
@@ -7,4 +19,37 @@ apiRouter.get("/", (req, res, next) => {
     });
 });
 
+apiRouter.use(async (req, res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+    if (!auth) { // nothing to see here
+        next();
+    } else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length);
+a s
+        try {
+            const { id } = jwt.verify(token, JWT_SECRET);
+            if (id) {
+                req.user = await getUserById(id);
+                next();
+            }
+        } catch ({ name, message }) {
+            next({ name, message });
+        }
+    } else {
+        next({ 
+            name: 'AuthorizationHeaderError',
+            message: `Authorization token must start with ${ prefix }`
+        });
+    }
+});   
+
+
+
+// Error Handler
+apiRouter.use((error, req, res, next) => {
+    res.send(error);
+});
+
+// Exports
 module.exports = apiRouter;
