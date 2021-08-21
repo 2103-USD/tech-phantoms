85557e8c0728ab3ca586f54c8cb06443c2c73a8b
Matthew <vera.matthew@gmail.com>
2021-08-21 11:05:22 -0700

    removed JS files

---
+++ src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { storeCurrentUser } from '../auth';
+
+const Login = ({ setUser }) => {
+	const [form, setForm] = useState({ username: '', password: '' });
+
+	const handleInput = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(
+				'',
+				{
+					username: form.username,
+					password: form.password,
+				}
+			);
+
+			setUser(res.data.user);
+			storeCurrentUser(res.data.user, res.data.token);
+			alert('Welcome to Fitness Trackr, ', res.data.user);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Login</h1>
+
+			<form onSubmit={handleSubmit}>
+				<label>Username</label>
+				<input name="username" value={form.username} onInput={handleInput} />
+				<label>Password</label>
+				<input
+					name="password"
+					value={form.password}
+					onInput={handleInput}
+					type="password"
+				/>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as App } from './App';
-export { default as Products } from './Product';
-export { default as AllProducts } from './AllProducts';
