673f92043e5695d9837c286d75197ad9641f5254
Matthew <vera.matthew@gmail.com>
2021-08-21 13:09:56 -0700

    merging with dev and stashing these

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as App } from './App';
 export { default as Products } from './Product';
 export { default as AllProducts } from './AllProducts';
+export { default as NavBar } from './NavBar'
