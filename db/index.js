29cbb56d7eaf2ad35d51fb261386e7afc9cd0b34
Jonathon Guertin <guertinj@cajonvalley.net>
2021-08-23 11:18:00 -0700

    User DB adapters

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 // Connect to DB
 const { Client } = require("pg");
-const DB_NAME = "change-this-name";
-const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
+const DB_NAME = process.env.DATABASE_NAME || `techphantomsdb`;
+const DB_URL =
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
@@ -10,4 +13,5 @@ const client = new Client(DB_URL);
 module.exports = {
   client,
   ...require("./products"),
+  ...require("./users"),
 };
