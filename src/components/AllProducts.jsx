4c27ad6a2dc625ce5da0e14eabf5915c7fb3b0da
Matthew <vera.matthew@gmail.com>
2021-08-24 16:22:00 -0700

    Complete Update of SRC
    Updated API, AUTH, and Components folders.

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,30 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Product } from '.';
-import {getProducts} from '../api'
-
+import Product from './Product';
+import { getAllProducts } from '../api';
+import './style.css';
 
 const AllProducts = () => {
-// Let's fetch some products and render them
-
 	const [products, setProducts] = useState([]);
 
 	const handleProducts = async () => {
-		const res = await getProducts();
-		if (res) setProducts(res);
+		const res = await getAllProducts();
+		if (res.length > 0) setProducts(res);
 	};
 
-    useEffect(() => {
-        handleProducts();
-    }, [])
-
+	useEffect(() => {
+		handleProducts();
+	}, []);
 
 	return (
 		<div className="allProducts">
 			<h1>Products:</h1>
 			<div>
-				{!!products.length && products.map((product) => (
-					<Product key={product.id} product={product} />
-				))}
+				{products.length &&
+					products.map((product) => {
+						return <Product key={product.id} product={product} />;
+					})}
 			</div>
 		</div>
 	);
---
+++ src/components/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { storeCurrentUser } from '../auth';
+import './style.css';
+
+const Login = ({ setUser }) => {
+	const [form, setForm] = useState({ username: '', password: '' });
+
+	const handleInput = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post('', {
+				username: form.username,
+				password: form.password,
+			});
+
+			setUser(res.data.user);
+			storeCurrentUser(res.data.user, res.data.token);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Login</h1>
+
+			<form onSubmit={handleSubmit}>
+				<label>Username</label>
+				<input name="username" value={form.username} onInput={handleInput} />
+				<label>Password</label>
+				<input
+					name="password"
+					value={form.password}
+					onInput={handleInput}
+					type="password"
+				/>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
