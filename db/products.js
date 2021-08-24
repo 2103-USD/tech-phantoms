6fb083e3b5e5103f8e0992fe54fc176fc69d3642
jonguertin <81204092+jonguertin@users.noreply.github.com>
2021-08-23 20:43:54 -0700

    added exports

--- db/products.js
+++ db/products.js
@@ -2,58 +2,64 @@ const client = require("./index");
 
 //get product by id
 export async function getProductById(id) {
-  try {
-    const {
-      rows: [product],
-    } = await client.query(
-      `
+    try {
+        const {
+            rows: [product],
+        } = await client.query(
+            `
       SELECT *
       FROM products
       WHERE id = $1;
       `,
-      [id]
-    );
-    return product;
-  } catch (error) {
-    throw Error(error);
-  }
+            [id]
+        );
+        return product;
+    } catch (error) {
+        throw Error(error);
+    }
 }
 
 export async function getAllProducts() {
-  try {
-    const { rows: products } = await client.query(
-      `
+    try {
+        const { rows: products } = await client.query(
+            `
       SELECT *
       FROM products;
       `
-    );
-    return products;
-  } catch (error) {
-    throw Error(error);
-  }
+        );
+        return products;
+    } catch (error) {
+        throw Error(error);
+    }
 }
 
 export async function createProduct({
-  name,
-  description,
-  price,
-  imageURL,
-  inStock,
-  category,
+    name,
+    description,
+    price,
+    imageURL,
+    inStock,
+    category,
 }) {
-  try {
-    const {
-      rows: [product],
-    } = await client.query(
-      `
+    try {
+        const {
+            rows: [product],
+        } = await client.query(
+            `
       INSERT INTO products (name, description, price, "imageURL", "inStock", category)
       VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *;
       `,
-      [name, description, price, imageURL, inStock, category]
-    );
-    return product;
-  } catch (error) {
-    throw Error(error);
-  }
+            [name, description, price, imageURL, inStock, category]
+        );
+        return product;
+    } catch (error) {
+        throw Error(error);
+    }
 }
+
+module.exports = {
+    getProductById,
+    getAllProducts,
+    createProduct,
+};
