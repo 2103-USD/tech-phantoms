2103ed50bfd72d46c29e0015359da0230b9042af
jonguertin <81204092+jonguertin@users.noreply.github.com>
2021-09-02 04:50:04 +0200

    update single product

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { getProduct } from '../api';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { getProduct } from "../api";
+import { useParams } from "react-router-dom";
 
-import './style.css';
+import "./style.css";
 
 export const SingleProduct = (props) => {
-	const [product, setProduct] = useState([]);
-	const { productId } = useParams();
+    const [product, setProduct] = useState([]);
+    const { productId } = useParams();
+    console.log("are we getting the productId =>", productId);
 
-	console.log('this should get something', getProduct(productId));
+    const product = await getProduct(productId);
 
-	useEffect(() => {
-		getProduct(productId);
-	}, []);
+    console.log("this is the product we fetched => ", product);
 
-	const { id, name, description, price, imageURL, inStock, category } =
-		product;
+    const { id, name, description, price, imageURL, inStock, category } =
+        product;
 
-	console.log('this is the product', product);
+    console.log("this is the product", product);
 
-	return (
-		<div id={`singleProduct${id}`} className="product-card">
-			<h3>{name}</h3>
-			<img src={imageURL} alt="product" />
-			<h4>Product Information</h4>
-			<p>Category: {category}</p>
-			<p>description: {description}</p>
-			<p>Price: ${price}</p>
-			<p>In Stock: {inStock}</p>
-		</div>
-	);
+    return (
+        <div id={`singleProduct${id}`} className="product-card">
+            <h3>{name}</h3>
+            <img src={imageURL} alt="product" />
+            <h4>Product Information</h4>
+            <p>Category: {category}</p>
+            <p>description: {description}</p>
+            <p>Price: ${price}</p>
+            <p>In Stock: {inStock}</p>
+        </div>
+    );
 };
 
 export default SingleProduct;
