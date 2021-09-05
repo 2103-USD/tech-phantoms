90502a728ba36b5efb3b785317a7448949af718e
VW71superbeetle <13732919+VW71superbeetle@users.noreply.github.com>
2021-09-04 23:34:42 -0700

    Order/OrderProducts support
    Create REST API calls for orders and order_products. Expand returned data for getOrderProductByID

---
+++ src/api/orders.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import {BASE_URL, getHeaders} from './auth'
+
+const SectionURL = `${BASE_URL}/orders`;
+
+//************* Order Functions
+export async function getAllOrders() {
+    const URL = `${SectionURL}/`
+	try {
+		const {data} = await axios.get(`${URL}`,
+            getHeaders());
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
+
+export async function createNewOrder(status) {
+    const URL = `${SectionURL}/`
+    try {
+        const {data} = await axios.post(`${URL}`,{
+            status
+        }, getHeaders());
+        return data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function getOpenCart() {
+    const URL = `${SectionURL}/cart`
+	try {
+		const {data} = await axios.get(`${URL}`,
+            getHeaders());
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
+
+export async function addItemToCart(productId, price, quantity, orderId) {
+    const URL = `${SectionURL}/${orderId}/products`
+    try {
+        const {data} = await axios.post(`${URL}`,{
+            productId,
+            price,
+            quantity
+        }, getHeaders());
+        return data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function updateOrderStatus(orderId, status) {
+    const URL = `${SectionURL}/${orderId}`
+    try {
+        const {data} = await axios.patch(`${URL}`,{
+            status
+        }, getHeaders());
+        return data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function cancelOrder(orderId) {
+    const URL = `${SectionURL}/${orderId}`
+    try {
+        const {data} = await axios.delete(`${URL}`,
+            getHeaders());
+        return data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
