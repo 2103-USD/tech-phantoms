5c24527908bf189fe757255cf0aa149797ba6201
Matthew <vera.matthew@gmail.com>
2021-09-11 11:08:15 -0700

    add to cart features updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,70 +1,71 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import {clearCurrentUser} from '../api/auth'
-import "./style.css";
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { clearCurrentUser } from '../api/auth';
+import './style.css';
 
 export const NavBar = ({ user, setUser }) => {
+	const history = useHistory();
 
-    const history = useHistory();
-    
-    const handleLogOut = async (e) => {
-        e.preventDefault();
-        try {
-            clearCurrentUser()
-            setUser(null);
-            history.push("/")
-        } catch (error) {
-            console.error(error)
-        }
-    }
+	const handleLogOut = async (e) => {
+		e.preventDefault();
+		try {
+			clearCurrentUser();
+			setUser(null);
+			history.push('/');
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    return (
-        <nav className="nav-bar">
-            <NavLink to="/" className="nav-item">
-                Home
-            </NavLink>
-            <NavLink to="/products" className="nav-item">
-                Products
-            </NavLink>
-            { user ?
-                (
-                    <>
-                        { (user.isAdmin) ?
-                            (
-                                <>
-                                    <NavLink to="/Admin/Products" className="nav-item">
-                                        Manage Products
-                                    </NavLink>
-                                    <NavLink to="/Admin/Orders" className="nav-item">
-                                        View All Orders
-                                    </NavLink>
-                                    <NavLink to="/Admin/Users" className="nav-item">
-                                        View All Users
-                                    </NavLink>
-                                </>
-                            )
-                            : ""
-                        }
+	return (
+		<nav className="nav-bar">
+			<NavLink to="/" className="nav-item">
+				Home
+			</NavLink>
+			<NavLink to="/products" className="nav-item">
+				Products
+			</NavLink>
+			{user ? (
+				<>
+					{user.isAdmin ? (
+						<>
+							<NavLink to="/Admin/Products" className="nav-item">
+								Manage Products
+							</NavLink>
+							<NavLink to="/Admin/Orders" className="nav-item">
+								View All Orders
+							</NavLink>
+							<NavLink to="/Admin/Users" className="nav-item">
+								View All Users
+							</NavLink>
+						</>
+					) : (
+						''
+					)}
 
-                        <Link to="/" className="nav-item" onClick={handleLogOut}>
-                            Logout
-                        </Link>
-                        <h1 className="nav-item">Welcome back, {user.username}</h1>
-                    </>
-                )
-                : (
-                    <>
-                        <NavLink to="/login" className="nav-item">
-                            Login
-                        </NavLink>
-                        <NavLink to="/register" className="nav-item">
-                            Register
-                        </NavLink>
-                    </>
-                )
-            }
-            
-        </nav>
-    );
+					<Link to="/" className="nav-item" onClick={handleLogOut}>
+						Logout
+					</Link>
+					<h1 className="nav-item">Welcome back, {user.username}</h1>
+					<NavLink to="/cart">
+						<img
+							className="shopping-cart"
+							src={require('../img/shopping_cart.png')}
+							alt="Shopping Cart"
+						/>
+					</NavLink>
+				</>
+			) : (
+				<>
+					<NavLink to="/login" className="nav-item">
+						Login
+					</NavLink>
+					<NavLink to="/register" className="nav-item">
+						Register
+					</NavLink>
+				</>
+			)}
+		</nav>
+	);
 };
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,50 +1,52 @@
-import React from "react";
-
-import { Link } from "react-router-dom";
-
-import "./style.css";
+import React from 'react';
+import { addItemToCart, GetCurrentCart } from '../api';
+import { useParams, Link } from 'react-router-dom';
+import './style.css';
 
 export const ProductCard = ({ product }) => {
-    const { id, name, description, price, imageURL, inStock, category } =
-        product;
 
-    return (
-        <div id={`product${id}`} className="product-card">
-            <Link
-                to={`/product/${id}`}
-                style={{
-                    textDecoration: "none",
-                    color: "#053F5F",
-                }}
-            >
-                <h3>{name}</h3>
-            </Link>
+	const { id, name, description, price, imageURL, inStock, category } =
+		product;
 
-            <img src={imageURL} alt="product" />
-            <h4>Product Information</h4>
-            <p>Category: {category}</p>
-            <p>Description: {description}</p>
-            <p>Price: ${price}</p>
-            <p>In Stock: {inStock}</p>
-            <br></br>
-            <span>Quantity</span>
-            <button
-                className="quantity-button"
-                style={{ backgroundColor: "#84f01e" }}
-            >
-                +
-                {/* // onClick={() => props.updateCartHandler(product.id, +1)}>  */}
-            </button>
+        const productId = product.id
 
-            {/* {product.qty} */}
+	const addProductToCart = async () => {
+       const cartItem = await addItemToCart(productId, price, '1', GetCurrentCart());
+		console.log('cartitem', cartItem)
+       if(cartItem) {
+        alert('Product successfully added to cart!') 
+     } else {
+         alert('Error adding product to cart!')
+     }
+	};
 
-            <button
-                className="quantity-button"
-                style={{ backgroundColor: "#f14e4e" }}
-                //   onClick={() => props.updateCartHandler(product.id, -1)}
-            >
-                -
-            </button>
-        </div>
-    );
+	return (
+		<div id={`product${id}`} className="product-card">
+			<Link
+				to={`/product/${id}`}
+				style={{
+					textDecoration: 'none',
+					color: '#053F5F',
+				}}
+			>
+				<h3>{name}</h3>
+			</Link>
+
+			<img src={imageURL} alt="product" />
+			<h4>Product Information</h4>
+			<p>Category: {category}</p>
+			<p>Description: {description}</p>
+			<p>Price: ${price}</p>
+			<p>In Stock: {inStock}</p>
+			<br></br>
+			<span>Add to Cart</span>
+			<button
+				className="quantity-button"
+				style={{ backgroundColor: '#84f01e' }}
+                onClick={addProductToCart}
+			>
+				+
+			</button>
+		</div>
+	);
 };
