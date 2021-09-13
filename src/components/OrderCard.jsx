52efda0619d31b63235cf1fad1fbd8e22f183683
Matthew <vera.matthew@gmail.com>
2021-09-12 23:19:43 -0700

    added timestamp format using moment

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import moment from 'moment';
 import { GetCurrentUser } from "../api";
 import "./style.css";
 
 export const OrderCard = ({ order }) => {
     const { id, status, firstName, lastName, email, datePlaced, products} = order;
     const user = GetCurrentUser();
-
+    const date = moment(datePlaced).format('MMMM Do YYYY')
     return (
         <>
             {(user.lastName === lastName) ? (
                 <div id={`${id}`} className="admin-order-card">
-                    <h3>Order #: {id} Placed on: {datePlaced}</h3>
+                    <h3>Order #: {id} Placed on: {date}</h3>
                     <h3>Ordered By: {lastName}, {firstName}</h3>
                     <h3>{email}</h3>
                     <h3>Status: {status}</h3>
