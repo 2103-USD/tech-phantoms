544d1d36dad05780d5588f03615e5dfed46149e2
Derek D <81129614+demedukd@users.noreply.github.com>
2021-09-12 21:17:15 -0700

    Added Search bar (Google redirect) and more CSS
    CSS
    Fixed footer. Now stays at bottom of page, but scrolling is required to view it.
    
    NavBar
    Added Google search

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { clearCurrentUser } from "../api/auth";
+import queryString from "query-string";
 import "./style.css";
 
 export const NavBar = ({ user, setUser }) => {
     const history = useHistory();
+    const [search, setSearch] = useState([]);
+    const google = "https://www.google.com/search?q=site%3A";
+    const site = "https://fast-savannah-33549.herokuapp.com/";
+
+    const initiateSearch = async ({ query }) => {
+        const searchString = queryString.parse(query.query);
+        console.log(searchString);
+        const url = google + site + "+" + query;
+        const win = window.open(url, "_blank");
+        win.focus();
+    };
 
     const handleLogOut = async (e) => {
         e.preventDefault();
@@ -49,6 +61,21 @@ export const NavBar = ({ user, setUser }) => {
                     <Link to="/" className="nav-item" onClick={handleLogOut}>
                         Logout
                     </Link>
+                    <input
+                        type="text"
+                        placeholder="Search . . ."
+                        onChange={(event) => {
+                            setSearch({ query: event.target.value });
+                        }}
+                        onKeyPress={(event) => {
+                            if (event.key === "Enter") {
+                                initiateSearch(search);
+                            }
+                        }}
+                        className="nav-item"
+                        id="search"
+                    ></input>
+
                     <h1 className="nav-title">
                         Welcome back, {user.firstName}!
                     </h1>
@@ -69,6 +96,12 @@ export const NavBar = ({ user, setUser }) => {
                     <NavLink to="/register" className="nav-item">
                         Register
                     </NavLink>
+                    <input
+                        type="text"
+                        placeholder="Search . . ."
+                        className="nav-item"
+                        id="search-no-login"
+                    ></input>
                 </>
             )}
         </nav>
