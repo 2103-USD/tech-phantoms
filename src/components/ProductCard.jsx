05cb9d12acb9b5d098a45f03b2c56ecf58eb26ff
Derek D <81129614+demedukd@users.noreply.github.com>
2021-09-13 17:45:27 -0700

    More CSS
    Social link on products.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -26,52 +26,66 @@ export const ProductCard = ({ product, user }) => {
         }
     };
 
+    const pleaseLogin = async () => {
+        toast("Please login to add products to cart", { type: "error" });
+    };
 
-	const pleaseLogin = async () => {
-		toast('Please login to add products to cart', { type: 'error' });
-	};
+    return (
+        <div id={`product${id}`} className="product-card">
+            <Link
+                to={`/product/${id}`}
+                style={{
+                    textDecoration: "none",
+                    color: "#053F5F",
+                }}
+            >
+                <h3>{name}</h3>
+            </Link>
 
-
-	return (
-		<div id={`product${id}`} className="product-card">
-			<Link
-				to={`/product/${id}`}
-				style={{
-					textDecoration: 'none',
-					color: '#053F5F',
-				}}
-			>
-				<h3>{name}</h3>
-			</Link>
-
-			<img src={imageURL} alt="product" />
-			<h4>Product Information</h4>
-			<p>Category: {category}</p>
-			<p>Description: {description}</p>
-			<p>Price: ${price}</p>
-			<p>In Stock: {inStock}</p>
-			<br></br>
-			{user ? (
-				<>
-					<span>Add to Cart</span>
-					<button
-						className="quantity-button"
-						onClick={addProductToCart}
-					>
-						+
-					</button>
-				</>
-			) : (
-				<>
-					<span>Add to Cart</span>
-					<button
-						className="quantity-button"
-						onClick={pleaseLogin}
-					>
-						+
-					</button>
-				</>
-			)}
-		</div>
-	);
+            <img src={imageURL} alt="product" />
+            <h4>Product Information</h4>
+            <p>Category: {category}</p>
+            <p>Description: {description}</p>
+            <p>Price: ${price}</p>
+            <p>In Stock: {inStock}</p>
+            <br></br>
+            {user ? (
+                <>
+                    <span>Add to Cart</span>
+                    <button
+                        className="quantity-button"
+                        onClick={addProductToCart}
+                    >
+                        +
+                    </button>
+                </>
+            ) : (
+                <>
+                    <span>Add to Cart</span>
+                    <button className="quantity-button" onClick={pleaseLogin}>
+                        +
+                    </button>
+                </>
+            )}
+            <div>
+                <button className="twitter-hashtag-button">
+                    <a
+                        href={
+                            "https://twitter.com/intent/tweet?hashtags=CajonValley%2C&original_referer=https%3A%2F%2Fpublish.twitter.com%2F&text=" +
+                            name
+                        }
+                        data-show-count="false"
+                        className="twitter-hashtag-button"
+                    >
+                        Tweet #CajonValley
+                    </a>
+                    <script
+                        async
+                        src="https://platform.twitter.com/widgets.js"
+                        charset="utf-8"
+                    ></script>
+                </button>
+            </div>
+        </div>
+    );
 };
