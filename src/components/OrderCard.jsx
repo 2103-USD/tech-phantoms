2a31073ecc20ab50b56b275a92bac3ac61614950
Matthew <captnjak@users.noreply.github.com>
2021-09-15 18:54:43 -0700

    Revert "Front end9 13.2"

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import moment from "moment";
+import { GetCurrentUser } from "../api";
 import "./style.css";
 
 export const OrderCard = ({ order }) => {
-    console.log('shlweifwjef ORDER', order)
-    const { id, status, datePlaced, products, total, paymentURL } =
+    const { id, status, lastName, datePlaced, products, total, paymentURL } =
         order;
+    const user = GetCurrentUser();
     const date = moment(datePlaced).format("MMMM Do YYYY");
 
     return (
         <>
-            {order ? (
+            {user.lastName === lastName ? (
                 <div id={`${id}`} className="admin-order-card">
                     <h3>Order #: {id}</h3>
                     {datePlaced ? <h3>Placed on: {date}</h3> : ""}
-                    <h3>Status: {status ? status.toUpperCase() : ""}</h3>
+                    <h3>Status: {status.toUpperCase()}</h3>
                     {paymentURL ? (
                         <h3>
                             <a
